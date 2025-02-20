f2d0c4452b1e99f638d43c13f0e6f07bb11733cf
Ben Zion Fischer <benzionfisher@gmail.com>
2025-02-20 20:06:06 +0200

    add reviewers  partly

--- cmps/Stages.jsx
+++ cmps/Stages.jsx
@@ -1,12 +1,25 @@
 import { stageService } from "../services/stage.service.js"
+import { reviewerService } from "../services/reviewer.service.js"
+
 const { useEffect, useState } = React
 
 export function Stages({ job }) {
 
     const [stages, setStages] = useState([])
+    const [reviewers, setReviewers] = useState([
+        {  
+            id: "abcd123",
+            name: "Ben Fisher",
+        },
+        {  
+            id: "tyu456",
+            name: "Itzik Nehemia",
+        }
+    ])
 
     useEffect(() => {
         loadStages()
+        // loadReviewers()
     }, [])
 
     function loadStages() {
@@ -17,9 +30,20 @@ export function Stages({ job }) {
             })
     }
 
+    function loadReviewers() {
+        reviewerService.query()
+            .then(setReviewers)
+            .catch(err => {
+                console.log('Problems getting reviewers:', err)
+            })
+    }
+
     return (
         <article className="stages">
-            <div className="stages-reviewers">reviewers</div>
+            <div className="stages-reviewers">   
+                {reviewers.map((reviewer, index) => (
+                    <div key={index}>{index+1}. {reviewer.name}</div> // Display stage name
+                ))}</div>
             <div className="stages-lst">
                 {stages.map((stage, index) => (
                     <div key={index}>{index+1}. {stage.name}</div> // Display stage name
---
+++ services/reviewer.service.js
@@ -0,0 +1,57 @@
+import { loadFromStorage, makeId, saveToStorage } from './util.service.js'
+import { storageService } from './async-storage.service.js'
+
+const REVIEWER_KEY = 'reviewerDB'
+
+export const reviewService = {
+    query,
+    get,
+    remove,
+    save,
+    getDefaultFilter,
+    getFilterFromSrcParams
+}
+
+function query(filterBy = {}) {
+    return storageService.query(REVIEWER_KEY)
+        .then(reviewers => {
+            if (filterBy.companyId) {
+                reviewers = reviewers.filter(reviewer => reviewer.companyId == filterBy.companyId)
+            }
+            return reviewers
+        })
+}
+
+function get(reviewerId) {
+    return storageService.get(REVIEWER_KEY, reviewerId)
+        // .then(_setNextPrevCarId)
+}
+
+function remove(reviewerId) {
+    // return Promise.reject('Oh No!')
+    return storageService.remove(REVIEWER_KEY, reviewerId)
+}
+
+function save(reviewer) {
+    if (reviewer.id) {
+        return storageService.put(REVIEWER_KEY, reviewer)
+    } else {
+        return storageService.post(REVIEWER_KEY, reviewer)
+    }
+}
+
+// function getEmptyJob(title = '', description = ' ') {
+//     return { title, description }
+// }
+
+function getDefaultFilter() {
+    return { companyId: 1 }
+}
+
+
+function getFilterFromSrcParams(srcParams) {
+    const jobId = srcParams.get('jobId') || ''
+    return {
+        jobId
+    }
+}
