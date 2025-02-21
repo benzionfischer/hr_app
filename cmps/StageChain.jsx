e964de79d228b870c287591f5266c4a4a3622981
Ben Zion Fischer <benzionfisher@gmail.com>
2025-02-21 10:49:43 +0200

    use real stages

--- cmps/StageChain.jsx
+++ cmps/StageChain.jsx
@@ -1,25 +1,24 @@
 const { useEffect, useState } = React
 
 export function StageChain({ stages }) {
-    const [circles, setCircles] = useState(5); // Change dynamically
+    // const [circles, setCircles] = useState(5); // Change dynamically
 
     return (
         <div className="circle-chain">
-            {Array.from({ length: circles }).map((_, index) => (
+            {stages.map((_, index) => (
                 <React.Fragment key={index}>
                     {index === 0 && (
                         <div className="circle">
                             <img src="assets/img/phone.png" alt="Circle Image" />
                         </div>
                     )}   
-                    {index === circles-1  && (
+                    {index === stages.length-1  && (
                         <div className="circle">
                             <img src="assets/img/handshake.png" alt="Circle Image" />
                         </div>
                     )}
-                    {console.log(index)}
-                    {index != 0 && index != circles-1 && <div className="circle">{index}</div>}
-                    {index < circles - 1 && <div className="line"></div>} {/* Add line except after the last circle */}
+                    {index != 0 && index != stages.length-1 && <div className="circle">{index}</div>}
+                    {index < stages.length - 1 && <div className="line"></div>} {/* Add line except after the last circle */}
                 </React.Fragment>
             ))}
         </div>
