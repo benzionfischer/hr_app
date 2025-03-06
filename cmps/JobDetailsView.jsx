9d524705f45dc8bef6b285e51dae4880e090a854
Ben Zion Fischer <benzionfisher@gmail.com>
2025-03-06 19:02:15 +0200

    ..

--- cmps/JobDetailsView.jsx
+++ cmps/JobDetailsView.jsx
@@ -9,7 +9,7 @@ export function JobDetailsView({ job }) {
         <section className="job-details-view">
 
             <div className="job-detail-description">
-                <img src="/phone.png" alt="title" />
+                <img src={`${process.env.PUBLIC_URL}/phone.png`} alt="title" />
                 <h1>{job.title}</h1>
             </div>
 
